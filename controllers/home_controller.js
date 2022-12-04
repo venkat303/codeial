c75472ed0241d85a5d480149942efc741aa4e056
Raghavendra Bysani <raghava1171@gmail.com>
2022-12-04 20:45:23 +0530

    adding comments to DB and rendering them on browser

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -8,10 +8,18 @@ module.exports.home = function(req,res){
     //         posts:posts
     //     });
     // });
-    Post.find({}).populate('user').exec(function(err,user_posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate:{
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title: "Codeial | Home",
-            posts: user_posts
+            posts: posts
         });  
     })
     
