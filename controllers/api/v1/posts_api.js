693bce8c8b08128763f23738ad7513e909774f0f
Raghavendra Bysani <raghava1171@gmail.com>
2022-12-12 17:01:02 +0530

    adding file upload and making API & JWT authentication

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,44 @@
+let Post =require('../../../models/post');
+let Comment = require('../../../models/comment')
+
+module.exports.index = async function(req,res){
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate:{
+                path: 'user'
+            }
+        });
+
+    return res.json(200,{
+        message:'Lists of Post in V1',
+        posts: posts
+    })
+}
+
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+        if(post.user == req.user.id){
+            post.remove();
+            
+            await Comment.deleteMany({post:req.params.id});
+
+            return res.json(200,{
+                message: "Post and associated comments deleted successfully"
+            })
+        }else{
+            return res.json(401,{
+                message: "You cannot delete this post"
+            });
+        }
+    }catch(err){
+        return res.json(500,{
+            message:"Internal Server Error"
+        });
+    }
+    
+}
