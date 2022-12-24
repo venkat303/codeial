428b65f39e4e26d9b5431d0a6c2e1d2bd322350d
Raghavendra Bysani <raghava1171@gmail.com>
2022-12-24 22:46:09 +0530

    adding like option

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -18,6 +18,9 @@
                     // call the create comment class
                     new PostComments(data.data.post._id);
 
+                    // CHANGE :: enable the functionality of the toggle like button on the new post
+                    new ToggleLike($(' .toggle-like-button', newPost));
+
                     new Noty({
                         theme: 'relax',
                         text: "Post published!",
@@ -44,7 +47,13 @@
                         </small>
                         ${ post.content }
                         <br>
-                        <small>${ post.user.name}</small> 
+                        <small>${ post.user.name}</small>
+                        <br>
+                        <small>    
+                            <a class="toggle-like-button" data-likes="0" href="/likes/toggle/?id=${post._id}&type=Post">
+                                0 Likes
+                            </a>    
+                        </small>
                     </p>
                     <div class="post-comments">
                         <form action="/comments/create" method="post">
